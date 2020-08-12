10b45dfa5416caefcfcddfdb01c5db4217350623
enimo <enimong@gmail.com>
2020-08-12 18:38:28 +0800

    remove server debug info

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-process.env.DEBUG = "mediasoup*";  // add debug info 
+// process.env.DEBUG = "mediasoup*";  // add debug info 
 
 process.title = 'multiparty-meeting-server';
 
